639fe663a3836b836237bfaa412244a10dde059c
karamYaseen <karamyaseen725@gmail.com>
2023-08-15 20:42:02 +0300

    uploading

---
+++ Task 18/Test/LIB/STD_TYPES.h
@@ -0,0 +1,26 @@
+/*********************************************************************************************/
+/*************************** Name     :  Karam Yaseen   **************************************/
+/*************************** Data     :  --/--/2023     **************************************/
+/*************************** SWC      :  STD_LIB        **************************************/
+/*************************** E_Mail   :  karamyaseen725@gmail.com    *************************/
+/*************************** version  :  1.0            **************************************/
+/*********************************************************************************************/
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+typedef unsigned char       u8 ;
+typedef unsigned short int u16 ;
+typedef unsigned long  int u32 ;
+
+typedef signed char         s8 ;
+typedef signed short int   s16 ;
+typedef signed long  int   s32 ;
+
+typedef float               f32;
+typedef double              f64;
+
+#define INITIAL_VALUE         0
+
+#define NULL          ((void*)0)
+
+#endif
---
+++ Task 18/Test/MCAL/Timer/Timer_Interface.h
@@ -0,0 +1,44 @@
+/*********************************************************************************/
+/***************** Name    :   Karam Yaseen  *************************************/
+/***************** Date    :   11-08-2023    *************************************/
+/***************** SWC     :   Timer         *************************************/
+/***************** E_Mail   :  karamyaseen725@gmail.com    ***********************/
+/***************** Version :   1.0           *************************************/
+/*********************************************************************************/
+
+#ifndef TIMER_INTERFACE_H
+#define TIMER_INTERFACE_H
+
+#include "../../LIB/Error_State.h"
+#include "../../LIB/STD_TYPES.h"
+
+#define TIMER_NORMAL_MODE   	                        0
+#define TIMER_CTC_MODE	    	                        1
+#define TIMER_PWM_MODE                                  2
+#define TIMER_FAST_PWM_MODE                             3
+
+#define TIMER_NO_CLOCK_SOURCE	 	                    0
+#define TIMER_NO_PRESCALER_FACTOR 	                    1
+#define TIMER_DIVISION_FACTOR_8	 	                    2
+#define TIMER_DIVISION_FACTOR_64	                    3
+#define TIMER_DIVISION_FACTOR_256	                    4
+#define TIMER_DIVISION_FACTOR_1024	                    5
+#define TIMER_T0_EXTERNAL_CLOCK_SOURCE_FALLING	        6
+#define TIMER_T0_EXTERNAL_CLOCK_SOURCE_RISING	        7
+
+
+ES_t Timers_enu_Timer0Init                               (void);
+
+ES_t Timers_enu_TIMER0SetPreload                         (u8 Copy_u8_Preload);
+
+ES_t Timers_enu_TIMER0SetCTC                             (u8 Copy_u8_CTC);
+
+ES_t Timers_enu_TIMER0GetCounterValue                    (u8 * Copy_pu8_ReturnValue);
+
+ES_t Timers_enu_SetCallBack                              (void(*Copy_pf_CallBackFunc)(void),u8 Copy_u8_Mode);
+ 
+ES_t Timers_enu_Timer1Init                               (void);
+
+ES_t Timers_enu_SetServoAngle                            (u8 Copy_u8_ServoAngle);
+
+#endif
