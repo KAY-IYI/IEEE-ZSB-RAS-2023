b5f04f2c7c6d10ed8bef4d45c9cfdd4113b02813
karamYaseen <karamyaseen725@gmail.com>
2023-08-26 16:26:02 +0300

    update

--- Task 19/Test/HAL/Servo Motor/Servo_Interface.h
+++ Task 19/Test/HAL/Servo Motor/Servo_Interface.h
@@ -4,9 +4,9 @@
 #include "../../LIB/STD_TYPES.h"
 #include "../../LIB/Error_State.h"
 
-#define Servo_u8_PORT           DIO_u8_PORTD
+#define Servo_u8_PORT           DIO_u8_PORTA
 #define Servo_u8_pin            DIO_u8_PIN5
 
-ES_t Servo_enu_SetServoAngle(u8 Copy_u8_ServoAngle);
+ ES_t Servo_enu_SetServoAngle(u8 Copy_u8_ServoAngle);
 
 #endif
